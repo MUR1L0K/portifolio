5c26a1439ce0f0e04608dbead785d849cf9c62f9
Murilo Klein <91572912+MUR1L0K@users.noreply.github.com>
2023-09-28 11:05:14 -0300

    update

--- index.js
+++ index.js
@@ -7,50 +7,44 @@ window.addEventListener("scroll", function () {
   }
 });
 
-document.getElementById("themeButton").addEventListener("click", () => {
+document.getElementById("chk").addEventListener("change", () => {
   const body = document.querySelector("body");
   const root = document.querySelector(":root");
   if (body.dataset.theme == "dark") {
-    document.getElementById("homeButton").src = "images/botao-home.png";
-    document.getElementById("themeImg").src = "images/night-mode.png";
-    document.getElementById("locationImg").src = "images/location.png";
     document.getElementById("gitHubImg").src = "images/github.png";
     document.getElementById("linkedinImg").src = "images/linkedin.png";
     document.getElementById("gitHubImg02").src = "images/github.png";
     document.getElementById("linkedinImg02").src = "images/linkedin.png";
-    document.getElementById("linkImg01").src = "images/external-link.png";
-    document.getElementById("linkImg02").src = "images/external-link.png";
-    document.getElementById("linkImg03").src = "images/external-link.png";
-    document.getElementById("mailImg").src = "images/mail-icon.png";
-    document.getElementById("phoneImg").src = "images/phone-icon.png";
-    document.getElementById("copyImg01").src = "images/copy-icon.png";
-    document.getElementById("copyImg02").src = "images/copy-icon.png";
     root.style.setProperty("--backGroundOne", "#fff");
-    root.style.setProperty("--backGroundTwo", "#fff");
+    root.style.setProperty("--backGroundTwo", "#ededed");
     root.style.setProperty("--backGroundThree", "#111827");
+    root.style.setProperty("--backGroundFour", "#c8cbd2");
+    root.style.setProperty("--backGroundFive", "#0e142328");
     root.style.setProperty("--letterColorOne", "#172439");
     root.style.setProperty("--letterColorTwo", "#fff");
-    body.dataset.theme = "light";
+    root.style.setProperty("--letterColorThree", "#fff");
+    root.style.setProperty("--linkTextShadow", "rgb(0, 0, 0)");
+    root.style.setProperty("--downloadBackGround", "rgba(0, 0, 0, 0.776)");
+    root.style.setProperty("--socialLinksBackGround", "#2e2e2e44");
+
+    body.dataset.theme = "ligth";
   } else {
-    document.getElementById("homeButton").src = "images/botao-home-white.png";
-    document.getElementById("themeImg").src = "images/light-mode.png";
-    document.getElementById("locationImg").src = "images/location-white.png";
     document.getElementById("gitHubImg").src = "images/github-white.png";
     document.getElementById("linkedinImg").src = "images/linkedin-white.png";
     document.getElementById("gitHubImg02").src = "images/github-white.png";
     document.getElementById("linkedinImg02").src = "images/linkedin-white.png";
-    document.getElementById("linkImg01").src = "images/external-link-white.png";
-    document.getElementById("linkImg02").src = "images/external-link-white.png";
-    document.getElementById("linkImg03").src = "images/external-link-white.png";
-    document.getElementById("mailImg").src = "images/mail-icon-white.png";
-    document.getElementById("phoneImg").src = "images/phone-icon-white.png";
-    document.getElementById("copyImg01").src = "images/copy-icon-white.png";
-    document.getElementById("copyImg02").src = "images/copy-icon-white.png";
     root.style.setProperty("--backGroundOne", "#030712");
     root.style.setProperty("--backGroundTwo", "#111827");
     root.style.setProperty("--backGroundThree", "#fff");
+    root.style.setProperty("--backGroundFour", "#374151");
+    root.style.setProperty("--backGroundFive", "#0e1423");
     root.style.setProperty("--letterColorOne", "#fff");
     root.style.setProperty("--letterColorTwo", "#172439");
+    root.style.setProperty("--letterColorThree", "#030712");
+    root.style.setProperty("--linkTextShadow", "rgba(255, 255, 255, 0.8)");
+    root.style.setProperty("--downloadBackGround", "#ffffffc9");
+    root.style.setProperty("--socialLinksBackGround", "#ffffff44");
+
     body.dataset.theme = "dark";
   }
 });
